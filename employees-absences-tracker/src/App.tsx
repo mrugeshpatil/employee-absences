f3848d8e8090ee87812effa9b6fde854bdd39683
Mrugesh Patil <mrugesh.patil@livestockinformation.org.uk>
2024-11-08 14:02:09 +0000

    implemented app global state with redux toolkit

--- employees-absences-tracker/src/App.tsx
+++ employees-absences-tracker/src/App.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState, AppDispatch } from './redux/store/store';
+import { fetchEmployeeAbsences } from './api/employee-absences-data';
 import './App.css';
 
 function App() {
+  const dispatch = useDispatch<AppDispatch>();
+  const { employeeAbsenceData, loading, error } = useSelector((state: RootState) => state.employee);
+
+  useEffect(() => {
+    dispatch(fetchEmployeeAbsences());
+  }, [dispatch]);
+
   return (
     <div className="App">
       <h1>Employee absences tracker</h1>
+      {loading && <p>Loading...</p>}
+      {error && <p>Error: {error}</p>}
+      {
+        employeeAbsenceData.map(employee => {
+          console.log('====> employee', employee)
+          return null
+        })
+      }
     </div>
   );
 }
---
+++ employees-absences-tracker/src/api/employee-absences-data.ts
@@ -0,0 +1,12 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const fetchEmployeeAbsences = createAsyncThunk(
+  "employee/fetchEmployeeAbsences",
+  async () => {
+    const response = await axios.get(
+      "https://front-end-kata.brighthr.workers.dev/api/absences"
+    );
+    return response.data;
+  }
+);
